ca9ebd6e2d8f8e1356b5e3218eaf89387c94218c
Lucas-Conde <lucascondeferreira@gmail.com>
2021-10-18 22:53:33 -0300

    Ajustand posicionamento de logo no cabecalho

--- src/ui/components/surfaces/Header/Header.tsx
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Toolbar, Container } from '@mui/material'
 
 const Header: React.FC = () => {
   return (
-    <HeaderAppBar>
+    <HeaderAppBar position={'sticky'}>
       <Toolbar component={Container}>
         <HeaderLogo src={'/img/logos/logo.svg'} alt={'e-diaristas'} />
       </Toolbar>
